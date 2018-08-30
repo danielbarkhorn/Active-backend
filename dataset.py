7dc14026e4db3753cfd237cc7566f77256a26f24
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-08-30 17:50:42 -0400

    feature: adding to dataset and server to create the unlabeled points and pass them to frontend

--- dataset.py
+++ dataset.py
@@ -4,8 +4,8 @@ import pickle as p
 import random
 
 class Dataset:
-    def __init__(self, filename='./data/iris.csv', percentLabeled=0.25, initLabeling=True, numLabeled=0, names=['sepal length', 'sepal width', 'petal length', 'petal width', 'label']):
-        self.masterData = pd.read_csv(filename, names=names)
+    def __init__(self, filename='./data/iris.csv', percentLabeled=0.5, initLabeling=True, numLabeled=0, features=['sepal length', 'sepal width', 'petal length', 'petal width', 'label']):
+        self.masterData = pd.read_csv(filename, names=features)
         self.masterShape = self.masterData.shape
         self.labels = self.masterData[names[-1]].unique()
 
@@ -15,15 +15,33 @@ class Dataset:
             else:
                 labeled_shape = (int(self.masterShape[0]*percentLabeled), self.masterShape[1])
 
+<<<<<<< Updated upstream
             labeledInd = random.sample(range(self.masterShape[0]), labeled_shape[0])
             self.labeledData = {l : {feat : [] for feat in names[:-1]} for l in self.labels}
+=======
+            isLabeled = [False] * self.masterShape[0]
+            for i in random.sample(range(self.masterShape[0]), labeled_shape[0]):
+                isLabeled[i] = True
 
-            # Adding the labeled data
-            for ind in labeledInd:
-                label = self.masterData['label'][ind]
-                for feat in names[:-1]:
-                    self.labeledData[label][feat].append(self.masterData[feat][ind])
+            self.labels = self.masterData[features[-1]].unique()
+            self.labeledData = {l : {feat : [] for feat in features[:-1]} for l in self.labels}
+
+            self.unlabeledData = {feat : [] for feat in features[:-1]}
+
+            # Adding the labeled / unlabeled data
+            for ind in range(self.masterShape[0]):
+                if isLabeled[ind]:
+                    label = self.masterData['label'][ind]
+                    for feat in features[:-1]:
+                        self.labeledData[label][feat].append(self.masterData[feat][ind])
+                else:
+                    for feat in features[:-1]:
+                        self.unlabeledData[feat].append(self.masterData[feat][ind])
+>>>>>>> Stashed changes
+
 
     def getLabeledData(self):
         return self.labeledData
 
+    def getUnlabeledData(self):
+        return self.unlabeledData
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 import dataset
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -10,12 +11,16 @@ CORS(app)
 def restart():
     iris = dataset.Dataset()
     iris_labeled = iris.getLabeledData()
+    iris_unlabeled = iris.getUnlabeledData()
 
-    response = jsonify(iris_labeled)
+    iris_json = {'labeled': iris_labeled, 'unlabeled': iris_unlabeled}
+
+    response = jsonify(iris_json)
     response.status_code = 200
 
     return (response)
 
+<<<<<<< Updated upstream
 @app.route('/label', methods=['POST'])
 def label():
     iris = dataset.Dataset()
@@ -25,3 +30,9 @@ def label():
     response.status_code = 200
 
     return (response)
+=======
+# @app.route('/label', methods=['POST'])
+# def label():
+#     try:
+#         unlabeled
+>>>>>>> Stashed changes
